3cef450865bc50f57e963c7cf3fda83c9972be5a
durgakulla <57334208+serpentmed@users.noreply.github.com>
2020-04-29 13:20:49 -0500

    rooms can play separately

---
+++ README.md
@@ -0,0 +1,24 @@
+## Ricochet Robots
+Online multiplayer board game with multiple gamerooms and chat. Based on the board game designed by Alex Randolph, built with nodejs, socket.io, and express.
+
+##What the game does right now:
+- [x] User goes to website, sees landing page asking for roomID input
+- [x] They can type in a friend's room, or their own ID to create that room
+- [x] They are redirected to the game url, which can be shared with friends
+- [x] They are asked for a display name
+- [x] They can chat only with others in their room
+- [x] Users can use 1,2,3,4 keys to switch between pieces
+- [x] Users can move pieces using arrow keys
+- [x] Everyone should be able to control the only four pieces
+
+##Things to work on:
+- [] Add collisions with other pieces
+- [] Render persistent blocks at starting position
+- [] Random board generation, along with piece start location randomizer button
+- [] Add buttons so users can grab control, and show that in the chat
+- [] Add mobile friendly buttons for movement, and get rid of chat on mobile (probably)
+- [] Send only current gama data to sockets instead of all games
+- [] Improve movement code to include visual transitions
+- [] Make user elements pretty
+- [] Add more server messages to chat (player leaving, player taking control)
+- [] Add timer and other board game elements
--- app.js
+++ app.js
@@ -38,6 +38,7 @@ console.log("Server started.");
 //each game has keys:
 //1. socketsInGame    - users in the room
 //2. board          - level currently in play
+//3. currPiece       - piece being controlled
 //3. startingPositions - starting location for all pieces
 //4. piecePositions - current location for all pieces
 
@@ -68,7 +69,7 @@ app.get('/id-:channel', function(req, res) {
 
 //if the game doesn't already exist, start it. Run whenever a user visits a valid room url
 function createNewGame(roomid){
-    //TODO: add function to randomly generate a board the board randomly generated
+    //TODO: add function to randomly generate a board, to be run after a new game is created, and when users push a button
     board = [
     [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
     [1, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1],
@@ -87,9 +88,9 @@ function createNewGame(roomid){
     [1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1],
     [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
     ];
-    //TODO:add function to randomly pick the starting positions
+    //TODO:add function to randomly pick the starting positions, to be run after a new game is created, and when users push button
     startingPositions = {
-        red: [2,1],
+        red: [3,5],
         green: [7,7],
         blue: [12,7],
         yellow: [2,13],
@@ -99,6 +100,7 @@ function createNewGame(roomid){
     games[roomid] = {
         socketsInGame: [],
         board: board,
+        currPiece: 'red',
         startingPositions: startingPositions,
         piecePositions: startingPositions,
         //the startingpositions and the piece positions are the same
@@ -113,19 +115,18 @@ var displaynames = {};
 //this object holds the roomid connected to each user
 var usersRoomid = {};
 
-//recieve and transmit data from mulitple clients with socket.io
+////******************* SENDING AND RECEIVING DATA TO/FROM CLIENT *******************
 var io = require('socket.io')(serv,{});
 io.sockets.on('connection', function(socket){
-    Player.onConnect(socket);
-
-    //do all of this when a user connects to a game page:
+    //******************* ON USER CONNECT *******************
     //generate a random number to assign as a socket.id to the connecting user
     socket.id = Math.random();
     //put that number into the socket_list
     SOCKET_LIST[socket.id] = socket;
-    //also recieve the roomid from the client, so we can say that this user is in that room (add to room object in the game)
+    //also receive the roomid from the client, so we can say that this user is in that room (add to room object in the game)
     socket.on('sendRoomidToServer',function(data){
-        //data received is the roomid, so put that into the usersroomid object
+        //data received is the roomid, so put that into the usersroomid object, with key socket.id
+        //also put that in the list of socketsingame in the games object
         usersRoomid[socket.id] = data;
         if (!(data in games)){
             //run function: if the room doesn't already exist, start a new game in it
@@ -134,45 +135,7 @@ io.sockets.on('connection', function(socket){
         games[data].socketsInGame.push(socket.id);
     });
 
-    //do this when a user disconnects
-    socket.on('disconnect',function(){
-        roomid = usersRoomid[socket.id];
-        //do this if the roomid still exists (server refresh deletes all games so much check for this)
-        if(typeof games[roomid] !== 'undefined'){
-            //delete their socketid from the games object
-            games[roomid].socketsInGame = games[roomid].socketsInGame.filter(v => v !== socket.id);
-            //delete the room from games object if no other users are in it
-            if (games[roomid].socketsInGame === undefined || games[roomid].socketsInGame.length == 0){
-                delete games[roomid];
-            }
-        }
-        //delete the user from the socket_list
-        delete SOCKET_LIST[socket.id];
-
-
-
-
-        var lastpack = {
-            player:Player.update()
-        }
-        for(i in lastpack.player){
-            if(lastpack.player[i].id == socket.id)
-                level[lastpack.player[i].rowpos][lastpack.player[i].colpos] = 0;
-            else if(lastpack.player[i].id == socket.id + 1)
-                level[lastpack.player[i].rowpos][lastpack.player[i].colpos] = 0;
-            else if(lastpack.player[i].id == socket.id + 2)
-                level[lastpack.player[i].rowpos][lastpack.player[i].colpos] = 0;
-            else if(lastpack.player[i].id == socket.id + 3)
-                level[lastpack.player[i].rowpos][lastpack.player[i].colpos] = 0;
-        }
-        delete Player.list[socket.id];
-        delete displaynames[socket.id];
-        delete Player.list[socket.id+1];
-        delete Player.list[socket.id+2];
-        delete Player.list[socket.id+3];
-        Player.onDisconnect(socket);
-    });
-
+    //******************* DURING GAME *******************
     //when server gets displayname, tell client to show game board, and tell users in the room that this user joined
     socket.on('sendDisplaynameToServer',function(data){
         displaynames[socket.id] = data[0];
@@ -200,219 +163,80 @@ io.sockets.on('connection', function(socket){
         }
     });
 
-});
-
-
-
-
-var currChar = 1;
-var currPos = {};
-var level = [
-    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-    [1, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1],
-    [1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1],
-    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1],
-    [1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-    [1, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1],
-    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1],
-    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-    [1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1],
-    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-    [1, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 1, 0, 0, 0, 1],
-    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 1],
-    [1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-    [1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1],
-    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
-];
-
-var Entity = function(){
-    var self = {
-        colpos:4,
-        rowpos:2,
-        spdX:0,
-        spdY:0,
-        id:0,
-        pressingRight: false,
-        pressingLeft: false,
-        pressingUp: false,
-        pressingDown: false,
-        moving: false,
-    }
-    self.update = function(){
-        self.updatePosition();
-    }
-    self.updatePosition = function(){
-        self.colpos += self.spdX;
-        self.rowpos += self.spdY;
-        if(self.spdX == 1)
-            level[self.rowpos][self.colpos-1] = 0;
-        else if(self.spdX == -1)
-            level[self.rowpos][self.colpos+1] = 0;
-        else if(self.spdY == 1)
-            level[self.rowpos-1][self.colpos] = 0;
-        else if(self.spdY == -1)
-            level[self.rowpos+1][self.colpos] = 0;
-    }
-    self.updateSpd = function(){
-        if(self.pressingRight && self.moving == false){
-            self.spdX = 1;
-            self.moving = true;
-        }
-        if(self.spdX == 1 && level[self.rowpos][self.colpos + 1] == 1){
-            self.spdX = 0;
-            self.moving = false;
-        }
-        if(self.spdX == 1 && level[self.rowpos][self.colpos + 1] == 2){
-            self.spdX = 0;
-            self.moving = false;
-        }
-        if(self.pressingLeft && self.moving == false){
-            self.spdX = -1;
-            self.moving = true;
-        }
-        if(self.spdX == -1 && level[self.rowpos][self.colpos - 1] == 1){
-            self.spdX = 0;
-            self.moving = false;
-        }
-        if(self.spdX == -1 && level[self.rowpos][self.colpos - 1] == 2){
-            self.spdX = 0;
-            self.moving = false;
-        }
-        if(self.pressingUp && self.moving == false){
-            self.spdY = -1;
-            self.moving = true;
-        }
-        if(self.spdY == -1 && level[self.rowpos - 1][self.colpos] == 1){
-            self.spdY = 0;
-            self.moving = false;
-        }
-        if(self.spdY == -1 && level[self.rowpos - 1][self.colpos] == 2){
-            self.spdY = 0;
-            self.moving = false;
-        }
-        if(self.pressingDown && self.moving == false){
-            self.spdY = 1;
-            self.moving = true;
-        }
-        if(self.spdY == 1 && level[self.rowpos + 1][self.colpos] == 1){
-                self.spdY = 0;
-                self.moving = false;
-        }
-        if(self.spdY == 1 && level[self.rowpos + 1][self.colpos] == 2){
-                self.spdY = 0;
-                self.moving = false;
-        }
-    }
-    return self;
-}
-
-var Player = function(id){
-    var self = Entity();
-    self.id = id;
-    self.number = "" + Math.floor(10 * Math.random());
-    var super_update = self.update;
-    self.update = function(){
-        level[self.rowpos][self.colpos] = 2;
-        self.updateSpd();
-        super_update();
-    }
-
-    Player.list[id] = self;
-    return self;
-}
-
-
-Player.list = {};
-Player.onConnect = function(socket){
-    var player = Player(socket.id);
-    var player2 = Player(socket.id+1);
-    var player3 = Player(socket.id+2);
-    var player4 = Player(socket.id+3);
+    //when server receives a keypress (comes with roomid)
     socket.on('keyPress',function(data){
+        game = games[data.roomid];
+        board = games[data.roomid].board;
+        piecePosition = game.piecePositions[game.currPiece];
         if(data.inputId === 'one'){
-            currChar = 1;
+            games[data.roomid].currPiece = 'red';
         }
         else if(data.inputId === 'two'){
-            currChar = 2;
+            games[data.roomid].currPiece = 'green';
         }
         else if(data.inputId === 'three'){
-            currChar = 3;
+            games[data.roomid].currPiece = 'blue';
         }
         else if(data.inputId === 'four'){
-            currChar = 4;
+            games[data.roomid].currPiece = 'yellow';
         }
-
-        if(currChar==1){
-            if(data.inputId === 'left')
-                player.pressingLeft = data.state;
-            else if(data.inputId === 'right')
-                player.pressingRight = data.state;
-            else if(data.inputId === 'up')
-                player.pressingUp = data.state;
-            else if(data.inputId === 'down')
-                player.pressingDown = data.state;
+        else if(data.inputId === 'left'){
+            while(board[piecePosition[0]][piecePosition[1]-1] !== 1){
+                piecePosition[1] -= 1;
+            }
+            game.piecePositions[game.currPiece] = piecePosition;
         }
-        else if(currChar==2){
-            if(data.inputId === 'left')
-                player2.pressingLeft = data.state;
-            else if(data.inputId === 'right')
-                player2.pressingRight = data.state;
-            else if(data.inputId === 'up')
-                player2.pressingUp = data.state;
-            else if(data.inputId === 'down')
-                player2.pressingDown = data.state;
+        else if(data.inputId === 'right'){
+            while(board[piecePosition[0]][piecePosition[1]+1] !== 1){
+                piecePosition[1] += 1;
+            }
+            game.piecePositions[game.currPiece] = piecePosition;
         }
-        else if(currChar==3){
-            if(data.inputId === 'left')
-                player3.pressingLeft = data.state;
-            else if(data.inputId === 'right')
-                player3.pressingRight = data.state;
-            else if(data.inputId === 'up')
-                player3.pressingUp = data.state;
-            else if(data.inputId === 'down')
-                player3.pressingDown = data.state;
+        else if(data.inputId === 'up'){
+            while(board[piecePosition[0]-1][piecePosition[1]] !== 1){
+                piecePosition[0] -= 1;
+            }
+            game.piecePositions[game.currPiece] = piecePosition;
         }
-        else if(currChar==4){
-            if(data.inputId === 'left')
-                player4.pressingLeft = data.state;
-            else if(data.inputId === 'right')
-                player4.pressingRight = data.state;
-            else if(data.inputId === 'up')
-                player4.pressingUp = data.state;
-            else if(data.inputId === 'down')
-                player4.pressingDown = data.state;
+        else if(data.inputId === 'down'){
+            while(board[piecePosition[0]+1][piecePosition[1]] !== 1){
+                piecePosition[0] += 1;
+            }
+            game.piecePositions[game.currPiece] = piecePosition;
         }
+        console.log(game)
     });
-}
-Player.onDisconnect = function(socket){
-    delete Player.list[socket.id];
-    delete Player.list[socket.id+1];
-    delete Player.list[socket.id+2];
-    delete Player.list[socket.id+3];
-}
-Player.update = function(){
-    var pack = [];
-    for(var i in Player.list){
-        var player = Player.list[i];
-        player.update();
-        pack.push({
-            colpos:player.colpos,
-            rowpos:player.rowpos,
-            number:player.number,
-            id:player.id
-        });
-    }
-    return pack;
-}
 
-setInterval(function(){
-    var pack = {
-        player:Player.update(),
-        level:level,
-    }
-    for(var i in SOCKET_LIST){
-        var socket = SOCKET_LIST[i];
-        socket.emit('newPositions',pack);
-    }
-},50);
+    //at a fixed time interval, send each socket the game data for the room its in
+    setInterval(function(){
+        socket.emit('gameUpdate',games);
+        /*
+        for(var socketid in displaynames){
+            pack = games[usersRoomid[socketid]];
+            socket.emit('gameUpdate',pack);
+        }
+        */
+    },50);
+
+
+    //******************* ON USER DISCONNECT *******************
+    socket.on('disconnect',function(){
+        roomid = usersRoomid[socket.id];
+        //do this if the roomid still exists (server refresh deletes all games so much check for this)
+        if(typeof games[roomid] !== 'undefined'){
+            //delete their socketid from the games object
+            games[roomid].socketsInGame = games[roomid].socketsInGame.filter(v => v !== socket.id);
+            //delete the room from games object if no other users are in it
+            if (games[roomid].socketsInGame === undefined || games[roomid].socketsInGame.length == 0){
+                delete games[roomid];
+            }
+        }
+        //delete the user from the displaynames object
+        delete displaynames[socket.id];
+        //delete the user from the usersRoomid object
+        delete usersRoomid[socket.id];
+        //delete the user from the socket_list
+        delete SOCKET_LIST[socket.id];
+
+    });
+});
