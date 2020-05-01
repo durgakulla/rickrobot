a681ed90ab469c07d8dbe0091928cc633fd45e24
durgakulla <57334208+serpentmed@users.noreply.github.com>
2020-05-01 00:08:25 -0500

    fixed piece display bug

--- app.js
+++ app.js
@@ -453,7 +453,6 @@ function setGoalPosition(board, pieceStartPositions){
         color: thePiece,
         position: theGoal,
     }
-    console.log(goalColorPosition);
     return(goalColorPosition);
 }
 
